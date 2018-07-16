bf3f49e3d38ba92caadb5e37b009eba6b2176608
Harry Cortes <heri032095@gmail.com>
2018-07-16 13:19:20 -0400

    Add files via upload

---
+++ PalindromeCheckerApp/PalindromeCheckerApp/PalindromeCheckerMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PalindromeCheckerApp;
+using UnitTestPalindromeCheckers; 
+
+namespace PalindromeCheckerApp
+{
+    class PalindromeCheckerMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calling the testing methods for the PalindromeChecker method.");
+            Console.WriteLine("Thus far this program cannot handle palindromes with spaces in them.");
+            UnitTestPalindromeChecker.palindromeCheckerTesterOne();
+            UnitTestPalindromeChecker.palindromeCheckerTesterTwo();
+            UnitTestPalindromeChecker.palindromeCheckerTesterThree();
+
+            Console.Read();
+            
+        }
+    }
+}
