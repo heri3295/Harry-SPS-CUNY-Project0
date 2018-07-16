36ed5d18e3080aa69175e97a2630c4bfb05ebe16
Harry Cortes <heri032095@gmail.com>
2018-07-16 13:20:09 -0400

    Add files via upload

---
+++ PalindromeCheckerApp/ClassLibrary1/PalindromeCheckerClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PalindromeCheckerApp
+{
+    public static class PalindromeChecker
+    {
+        public static bool palindromeChecker(string firstWord, string secondWord)
+        {
+            for (int indexTwo = (secondWord.Length - 1), indexOne = 0; indexTwo > 0; indexTwo--, indexOne++)
+            {
+                char letOne = firstWord[indexOne];
+                char letTwo = secondWord[indexTwo];
+                if (!(letOne.Equals(letTwo))) { return false; }
+            }
+            return true;
+        }
+
+    }
+}
