7dd6c8d1eb2a8146b1dcd6c1fbcbc5da33140328
Harry Cortes <heri032095@gmail.com>
2018-07-16 13:18:07 -0400

    Add files via upload

---
+++ PalindromeCheckerApp/UnitTestProject1/UnitTestPalindromeChecker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PalindromeCheckerApp; 
+
+namespace UnitTestPalindromeCheckers
+{
+    [TestClass]
+    public static class UnitTestPalindromeChecker
+    {
+        [TestMethod]
+        public static void palindromeCheckerTesterOne()
+        {
+            string wordToTest = "A nut for a jar of tuna";
+            Console.WriteLine("Checking for " + wordToTest + ". ");
+            bool isPalindrome = PalindromeChecker.palindromeChecker(wordToTest, wordToTest);
+            Console.WriteLine("Is Palindrome?: " + isPalindrome);
+        }
+
+        [TestMethod]
+        public static void palindromeCheckerTesterTwo()
+        {
+            string wordToTest = "Borrow or rob";
+            Console.WriteLine("Checking for " + wordToTest + ". ");
+            bool isPalindrome = PalindromeChecker.palindromeChecker(wordToTest, wordToTest);
+            Console.WriteLine("Is Palindrome?: " + isPalindrome);
+        }
+        [TestMethod]
+        public static void palindromeCheckerTesterThree()
+        {
+            string wordToTest = "343";
+            Console.WriteLine("Checking for " + wordToTest + ". ");
+            bool isPalindrome = PalindromeChecker.palindromeChecker(wordToTest, wordToTest);
+            Console.WriteLine("Is Palindrome?: " + isPalindrome);
+        }
+    }
+}
